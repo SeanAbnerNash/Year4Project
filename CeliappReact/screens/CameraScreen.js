9d718f20225dbb924592b24b2cd15ca4009b86d7
SeanAbnerNash <c00217019@itcarlow.ie>
2020-03-25 00:38:30 +0000

    Set up workstation for Remote Work. Set Up Emulator to allow for camera use and testing. Improved Camera Screen to Display Camera Feed and capturing images.

--- CeliappReact/screens/CameraScreen.js
+++ CeliappReact/screens/CameraScreen.js
@@ -1,38 +1,69 @@
+import React, { PureComponent } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { RNCamera } from 'react-native-camera';
-import React, { Component } from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
 
 
-export default class CameraScreen extends Component {
+export default class CameraScreen extends PureComponent  {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
 
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 
-
-    render() {
-        return (
-            <View>
-            <Text>C</Text>
-            <RNCamera ref={ref => {this.camera = ref;}} style={{ flex: 1, width: '100%',}}>
-                
-            </RNCamera>
-          </View>
-        );
-      }
-
+  takePicture = async() => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
 }
-const styles = StyleSheet.create({
-    container:{
 
-    },
-  baseText: {
-    paddingLeft: 20,
-    paddingTop: 15,
-    fontFamily: 'Cochin',
-    textAlign: 'left',
-    lineHeight: 12,
-    fontSize: 18,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
   },
-  titleText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
   },
 });
